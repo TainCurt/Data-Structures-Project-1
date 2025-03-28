79c98422f95765f449bedcec787e9487a1daa606
Jakub Kulik <kulikoskyyy@gmail.com>
2025-03-29 00:33:18 +0100

    i can do anything

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -40,3 +40,61 @@ void LinkedList::pop_first()
 }
 
 
+void LinkedList::add_back(int value)
+{
+    Node* newNode = new Node;
+    newNode->data = value;
+    Node* temp = head;
+    while(temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void LinkedList::pop_back()
+{
+    Node* temp = head;
+    while(temp->next != tail )
+    {
+        temp = temp->next;
+    }
+    delete tail;
+    tail = temp;
+    tail->next = nullptr;
+}
+
+
+void LinkedList::add_at(int value, int key)
+{
+    if (key <= 0) {
+        return;
+    }
+
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = nullptr;
+
+    if (key == 1) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    int i = 1;
+
+    while (temp != nullptr && i < key - 1) {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == nullptr) {
+        delete newNode;
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
